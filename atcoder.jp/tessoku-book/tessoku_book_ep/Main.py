07eaabbe7f9352131e477b0deda04f3db438c897
shirororo <yangshiro4649@gmail.com>
2023-04-20 17:06:49 +0000

    [AC] tessoku-book tessoku_book_ep

---
+++ atcoder.jp/tessoku-book/tessoku_book_ep/Main.py
@@ -0,0 +1,85 @@
+# S->workers->times->T
+
+# Dinic's algorithm
+from collections import deque
+class Dinic:
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+
+    def bfs(self, s, t):
+        self.level = level = [None]*self.N
+        deq = deque([s])
+        level[s] = 0 # type: ignore
+        G = self.G
+        while deq:
+            v = deq.popleft()
+            lv = level[v] + 1
+            for w, cap, _ in G[v]:
+                if cap and level[w] is None:
+                    level[w] = lv
+                    deq.append(w)
+        return level[t] is not None
+
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        level = self.level
+        for e in self.it[v]:
+            w, cap, rev = e
+            if cap and level[v] < level[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self, s, t):
+        flow = 0
+        INF = 10**9 + 7
+        G = self.G
+        while self.bfs(s, t):
+            *self.it, = map(iter, self.G)
+            f = INF
+            while f:
+                f = self.dfs(s, t, INF)
+                flow += f
+        return flow
+    
+def main():
+    N, M = map(int, input().split())
+    nf = Dinic(N+26)
+    for i in range(N):
+        nf.add_edge(N+24, i, 10)
+    for j in range(24):
+        nf.add_edge(N+j, N+25, M)
+    
+    for i in range(N):
+        s = input()
+        for j in range(len(s)):
+            if s[j] == '1':
+                nf.add_edge(i, N+j, 1)
+    
+    ans = nf.flow(N+24, N+25)
+    if ans == 24*M:
+        print("Yes")
+    else:
+        print("No")
+        
+        
+        
+if __name__ == '__main__':
+    main()
