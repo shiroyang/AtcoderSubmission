9cb9100d60b3efadd4dc39b55c942e56ad357118
shirororo <yangshiro4649@gmail.com>
2023-05-05 03:40:09 +0000

    [AC] typical90 typical90_bw

---
+++ atcoder.jp/typical90/typical90_bw/Main.py
@@ -0,0 +1,22 @@
+from math import log2, ceil
+
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f*f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+
+N = int(input())
+A = prime_factorize(N)
+ans = ceil(log2(len(A)))
+print(ans)
