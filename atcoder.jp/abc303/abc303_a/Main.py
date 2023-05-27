48487e31fbd8d7a55730bbc590761bd1bd6e3288
shirororo <yangshiro4649@gmail.com>
2023-05-27 12:02:41 +0000

    [AC] abc303 abc303_a

---
+++ atcoder.jp/abc303/abc303_a/Main.py
@@ -0,0 +1,18 @@
+N = int(input())
+S = input()
+T = input()
+
+for i in range(N):
+    if S[i] == T[i]:
+        continue
+    elif S[i] == '1' and T[i] == 'l':
+        continue
+    elif S[i] == 'l' and T[i] == '1':
+        continue
+    elif S[i] == 'o' and T[i] == '0':
+        continue
+    elif S[i] == '0' and T[i] == 'o':
+        continue
+    exit(print("No"))
+
+print("Yes")    
